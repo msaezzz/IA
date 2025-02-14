e835228b77c71638670a1baaac5a88517db99e0c
msaezzz <2024_marc.saez@iticbcn.cat>
2025-02-14 19:29:05 +0100

    delete copy

--- Naturalcopy/tokenizer_test.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-
-text = "Hello, how are you doing today?"
-
-tokens = tokenizer.tokenize(text)
-
-print(tokens)
